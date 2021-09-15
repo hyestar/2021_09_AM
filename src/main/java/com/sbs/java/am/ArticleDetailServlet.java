857f45c2b76d75cdb92a756166cfb604d1ea5d45
hyestar <0109jhs@naver.com>
2021-09-16 00:25:31 +0900

    게시물 상세페이지 구현

--- src/main/java/com/sbs/java/am/ArticleDetailServlet.java
+++ src/main/java/com/sbs/java/am/ArticleDetailServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -13,8 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/article/list")
-public class ArticleListServlet extends HttpServlet {
+@WebServlet("/article/detail")
+public class ArticleDetailServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
@@ -40,13 +39,16 @@ public class ArticleListServlet extends HttpServlet {
 					conn = DriverManager.getConnection(url, user, password);
 					DBUtil dbUtil = new DBUtil(request, response);
 
-					String sql = "SELECT * FROM article ORDER BY id DESC";
-					List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
+					int id = Integer.parseInt(request.getParameter("id"));
+					
+					// String.format을 사용하면 C언어의 printf처럼 사용가능
+					String sql = String.format("SELECT * FROM article WHERE id = %d", id);
+					Map<String, Object> articleRow = dbUtil.selectRow(conn, sql);
 
-					response.getWriter().append(articleRows.toString());
+					response.getWriter().append(articleRow.toString());
 
-					request.setAttribute("articleRows", articleRows);
-					request.getRequestDispatcher("/jsp/home/list.jsp").forward(request, response);
+					request.setAttribute("articleRow", articleRow);
+					request.getRequestDispatcher("/jsp/home/detail.jsp").forward(request, response);
 
 				} catch (SQLException e) {
 					e.printStackTrace();
--- src/main/webapp/jsp/home/list.jsp
+++ src/main/webapp/jsp/home/list.jsp
@@ -1,10 +1,12 @@
-<%@ page import="java.util.List" %>
-<%@ page import="java.util.Map" %>
+<%@ page import="java.util.List"%>
+<%@ page import="java.util.Map"%>
 
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    
- <% List<Map<String, Object>> articleRows = (List<Map<String, Object>>) request.getAttribute("articleRows"); %>
+	pageEncoding="UTF-8"%>
+
+<%
+List<Map<String, Object>> articleRows = (List<Map<String, Object>>) request.getAttribute("articleRows");
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +14,7 @@
 <title></title>
 </head>
 <body>
-<h1>게시물 리스트 v1</h1>
+	<%-- <h1>게시물 리스트 v1</h1>
 <ul>
 	<li><%=(int) articleRows.get(0).get("id") %>번, <%=articleRows.get(0).get("regDate") %>, <%=(String) articleRows.get(0).get("title") %></li>
 	<li><%=(int) articleRows.get(1).get("id") %>번, <%=articleRows.get(1).get("regDate") %>, <%=(String) articleRows.get(1).get("title") %></li>
@@ -49,15 +51,19 @@
 	<%} %>
 	
 </ul>
+ --%>
+	<h1>게시물 리스트</h1>
+	<ul>
+		<%
+		for (Map<String, Object> articleRow : articleRows) {
+		%>
+		<li><a href="detail?id=<%=(int) articleRow.get("id")%>"><%=(int) articleRow.get("id")%>번, <%=articleRow.get("regDate")%>,
+				<%=(String) articleRow.get("title")%></a></li>
+		<%
+		}
+		%>
 
-<h1>게시물 리스트 v5</h1>
-<ul>
-
-	<%for(Map<String, Object> articleRow : articleRows){ %>
-	<li><%=(int) articleRow.get("id") %>번, <%=articleRow.get("regDate") %>, <%=(String)articleRow.get("title") %></li>	
-	<%} %>
-	
-</ul>
+	</ul>
 
 </body>
 </html>
